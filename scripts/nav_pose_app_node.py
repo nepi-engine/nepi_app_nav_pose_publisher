7b2575be438d757d3aacb7fe57d59a30051cd373
Jason Seawall <jseawall@numurus.com>
2025-05-17 18:11:08 -0700

    3p2p0-RC5G

--- scripts/nav_pose_app_node.py
+++ scripts/nav_pose_app_node.py
@@ -154,8 +154,7 @@ class NavPosePublisher(object):
                     configs_dict = self.CFGS_DICT,
                     params_dict = self.PARAMS_DICT,
                     pubs_dict = self.PUBS_DICT,
-                    subs_dict = self.SUBS_DICT,
-                    log_class_name = True
+                    subs_dict = self.SUBS_DICT
     )
 
     ready = self.node_if.wait_for_ready()
